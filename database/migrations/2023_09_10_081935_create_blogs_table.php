e50b0c183e77eac2d61bb58f526745ee6241fd2a
Arghadeep Halder <arghadeep@capitalnumbers.com>
2023-09-10 14:10:25 +0530

    blog model and migration

---
+++ database/migrations/2023_09_10_081935_create_blogs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->string('cover_image')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
